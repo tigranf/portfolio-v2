3679d2134370f973011bc184cd824867956a7a75
Tigran Farmanyan <tigran.farmanyan@gmail.com>
2023-02-03 11:18:30 -0600

    Resume download and tooltip in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,16 +1,25 @@
+import { Tooltip, Zoom } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="w-full max-w-4xl mx-auto font-bold text-black body-font">
       <div className="container flex flex-col items-center px-5 py-8 mx-auto sm:justify-around sm:flex-row">
-
-        <a
-          className="flex items-center justify-center font-medium text-black title-font md:justify-start sm:ml-auto"
-          href="https://github.com/tigranf"
+        <Tooltip
+          title="Download Resume"
+          TransitionComponent={Zoom}
+          followCursor
+          enterDelay={300}
+          leaveDelay={200}
+          placement="top"
         >
-          <span className="ml-3 text-2xl underline">Tigran Farmanyan</span>
-        </a>
+          <a
+            className="flex items-center justify-center font-medium text-black title-font md:justify-start sm:ml-auto"
+            href="/2023_Resume_Tigran_Farmanyan.pdf"
+          >
+            <span className="ml-3 text-2xl underline">Tigran Farmanyan</span>
+          </a>
+        </Tooltip>
 
         <p className="mt-4 font-medium text-black text-md sm:mx-4 sm:px-4 sm:border-l-2 sm:border-black sm:py-2 sm:mt-0">
           © 2023 tig.dev creations —
